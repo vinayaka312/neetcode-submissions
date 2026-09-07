93f33676195f4a8ceea80d73c6673e3218a9ec77
Vinayaka Bhat <44962464+vinayaka312@users.noreply.github.com>
2026-09-08 01:06:02 +0530

    Add: concatenation-of-array - submission-5

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-5.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[2*n];
+        for(int i = 0; i < n; i++) {
+            result[i] = result[i+n] = nums[i];
+        }
+        return result;
+    }
+}
